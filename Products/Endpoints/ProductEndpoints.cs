a4b34683ecc95b50d8a864045fa85eac44a938e2
Vincent Baaij <vnbaaij@outlook.com>
2025-05-18 14:57:56 +0200

    - Prep for demo

--- Products/Endpoints/ProductEndpoints.cs
+++ Products/Endpoints/ProductEndpoints.cs
@@ -6,7 +6,7 @@ namespace Products.Endpoints;
 
 public static class ProductEndpoints
 {
-    public static void MapProductEndpoints (this IEndpointRouteBuilder routes)
+    public static void MapProductEndpoints(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/api/Product");
 
@@ -17,7 +17,7 @@ public static class ProductEndpoints
         .WithName("GetAllProducts")
         .Produces<List<Product>>(StatusCodes.Status200OK);
 
-        group.MapGet("/{id}", async  (int id, ProductDataContext db) =>
+        group.MapGet("/{id}", async (int id, ProductDataContext db) =>
         {
             return await db.Product.AsNoTracking()
                 .FirstOrDefaultAsync(model => model.Id == id)
@@ -29,43 +29,5 @@ public static class ProductEndpoints
         .Produces<Product>(StatusCodes.Status200OK)
         .Produces(StatusCodes.Status404NotFound);
 
-        group.MapPut("/{id}", async  (int id, Product product, ProductDataContext db) =>
-        {
-            var affected = await db.Product
-                .Where(model => model.Id == id)
-                .ExecuteUpdateAsync(setters => setters
-                  .SetProperty(m => m.Id, product.Id)
-                  .SetProperty(m => m.Name, product.Name)
-                  .SetProperty(m => m.Description, product.Description)
-                  .SetProperty(m => m.Price, product.Price)
-                  .SetProperty(m => m.ImageUrl, product.ImageUrl)
-                );
-
-            return affected == 1 ? Results.Ok() : Results.NotFound();
-        })
-        .WithName("UpdateProduct")
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status204NoContent);
-
-        group.MapPost("/", async (Product product, ProductDataContext db) =>
-        {
-            db.Product.Add(product);
-            await db.SaveChangesAsync();
-            return Results.Created($"/api/Product/{product.Id}",product);
-        })
-        .WithName("CreateProduct")
-        .Produces<Product>(StatusCodes.Status201Created);
-
-        group.MapDelete("/{id}", async  (int id, ProductDataContext db) =>
-        {
-            var affected = await db.Product
-                .Where(model => model.Id == id)
-                .ExecuteDeleteAsync();
-
-            return affected == 1 ? Results.Ok() : Results.NotFound();
-        })
-        .WithName("DeleteProduct")
-        .Produces<Product>(StatusCodes.Status200OK)
-        .Produces(StatusCodes.Status404NotFound);
     }
 }
